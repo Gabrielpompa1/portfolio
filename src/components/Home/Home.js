57abc411729016027857d21811bec3fe9f91e54f
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-12-20 12:15:41 -0700

    Deploying to heroku

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './Home.css'
+import './Home.css';
 
 const Home = () => {
 	return (
 		<div className='home-div'>
-			<img className='home-image' src="/images/laptop.jpg" alt='' />
-			<div className='intro-div'>wasdf</div>
 		</div>
 	);
 };
